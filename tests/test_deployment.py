3eac76cc43479725460d155c0b0d1a553b2b0e12
Jakub Roztocil <jakub@roztocil.name>
2013-08-13 17:01:58 +0200

    Test remote URL.

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -161,6 +161,7 @@ class DeploymentVCSComponentsTest(_BaseDeploymentCommandTestCase):
             'version': None,
             'vcs': {
                 'vcs_type': 'git',
+                'remote_url': 'git@github.com:opbeat/opbeatcli.git'
             }
         })
         self.assertIsNotNone(package.vcs.branch)
@@ -223,6 +224,7 @@ class DeploymentVCSComponentsTest(_BaseDeploymentCommandTestCase):
                 'vcs': {
                     'vcs_type': 'subversion',
                     'branch': 'trunk',
+                    'remote_url': 'https://github.com/opbeat/opbeatcli/trunk'
                 }
             })
             self.assertIsNotNone(package.vcs.rev)
